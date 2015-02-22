f939ee781a1fcc0d058c2b6048abd45b87682da1
Jim Krooskos <krooskos@gmail.com>
2015-02-22 08:04:09 -0800

    change location of #! and unicode lines

--- nba_plot.py
+++ nba_plot.py
@@ -1,9 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 """
 Plot NBA Data for 2014-2015 
 Age by position box plots
 """
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
 
 import os
 import sys
